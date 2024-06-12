3a159f30c36c8faad1742d3d541c244aa7fdeeb5
Colin Huber <cbhuber@gmail.com>
2024-06-11 21:18:06 -0600

    feat: implement snake

--- src/snake.rs
+++ src/snake.rs
@@ -33,3 +33,19 @@ pub struct Snake {
     body: LinkedList<Block>,
     tail: Option<Block>,
 }
+
+impl Snake {
+    pub fn new(x: i32, y: i32) -> Snake {
+        let mut body: LinkedList<Block> = LinkedList::new();
+
+        body.push_back(Block { x: x + 2, y });
+        body.push_back(Block { x: x + 1, y });
+        body.push_back(Block { x, y });
+
+        Snake {
+            direction: Direction::Right,
+            body,
+            tail: None,
+        }
+    }
+}
